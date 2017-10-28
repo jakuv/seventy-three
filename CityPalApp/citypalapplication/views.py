8b05bd45630e15a4a193708ce2317cad6f53d9c0
treidy <tpreidy@gmail.com>
2017-10-28 15:54:14 +1000

    jinja and all that

--- CityPalApp/citypalapplication/views.py
+++ CityPalApp/citypalapplication/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.form import UserCreationForm
 from templates.forms import SignUpForm
 
 # Create your views here.
@@ -9,14 +10,25 @@ def home(request):
     return render(request, 'landingpage/home.html')
 
 
+# def register(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('landingpage/index.html')
+#     else:
+#         form = SignUpForm()
+#
+#         args = {'form': form}
+#         return render{request, ''}
+
 def register(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('landingpage/index.html')
     else:
-        form = SignUpForm()
-
+        form = UserCreationForm()
         args = {'form': form}
-        return render{request, ''}
+        return render(request, 'landingpage/signup.html', args)
